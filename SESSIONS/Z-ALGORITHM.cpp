77e05882ef4e570e8f720c9e1f412587d9b83d99
aaditya-anand-singh <singhaadityaanand@gmail.com>
2026-02-27 10:59:07 +0530

    cc experiments

--- SESSIONS/Z-ALGORITHM.cpp
+++ SESSIONS/Z-ALGORITHM.cpp
@@ -1,26 +1,51 @@
-#include <iostream>
-#include <unordered_map>
+#include <bits/stdc++.h>
 using namespace std;
 
+vector<int> computeZ(string s) {
+    int n = s.length();
+    vector<int> z(n, 0);
+
+    int L = 0, R = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (i <= R)
+            z[i] = min(R - i + 1, z[i - L]);
+
+        while (i + z[i] < n && s[z[i]] == s[i + z[i]])
+            z[i]++;
+
+        if (i + z[i] - 1 > R) {
+            L = i;
+            R = i + z[i] - 1;
+        }
+    }
+    return z;
+}
+
 int main() {
-    int n;
-    cin >> n;
+    string text, pattern;
 
-    unordered_map<string, int> freq;
-    string team, winner;
-    int maxCount = 0;
+    cout << "Enter Text: ";
+    cin >> text;
 
-    for(int i = 0; i < n; i++) {
-        cin >> team;
-        freq[team]++;
-        
-        if(freq[team] > maxCount) {
-            maxCount = freq[team];
-            winner = team;
+    cout << "Enter Pattern: ";
+    cin >> pattern;
+
+    string concat = pattern + "$" + text;
+    vector<int> z = computeZ(concat);
+
+    bool found = false;
+
+    for (int i = 0; i < z.size(); i++) {
+        if (z[i] == pattern.length()) {
+            cout << "Pattern found at index: " 
+                 << i - pattern.length() - 1 << endl;
+            found = true;
         }
     }
 
-    cout << winner << endl;
+    if (!found)
+        cout << "Pattern not found";
 
     return 0;
 }
